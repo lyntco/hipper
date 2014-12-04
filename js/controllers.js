67e5b428a3e9a9f0021aede05b6023473c08b784
lyntco <strawww@gmail.com>
2014-12-04 12:57:55 +1100

    Start it up

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,25 @@
 angular.module('hipstrchat')
-  .controller('AppCtrl', function(){
-    console.log('app ctrl is loaded');
+  // .controller('AppCtrl', function($scope){
+  //   console.log('app ctrl is loaded');
+  //   $scope.title = "heroes";
+
+  //   $scope.heroes = [
+  //     'Superman',
+  //     'Batman',
+  //     'Catwoman',
+  //     'Spiderman'
+  //     ];
+
+  //   $scope.buttonClicked = function () {
+  //     alert('You clicked this button');
+  //   };
+  // })
+  .controller('HeaderCtrl', function($scope){
+    $scope.title = "This is the header";
+  })
+  .controller('SidebarCtrl', function($scope){
+    $scope.title = "This is the sidebar";
+  })
+  .controller('MessagesCtrl', function($scope){
+    $scope.title = "This is the messages";
   });
