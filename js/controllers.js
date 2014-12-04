36682a92634652289e6cbc9a6dbe60323175e2ff
lyntco <strawww@gmail.com>
2014-12-04 15:04:07 +1100

    Why you no ng-repeat

--- js/controllers.js
+++ js/controllers.js
@@ -2,14 +2,12 @@ angular.module('hipstrchat')
   // .controller('AppCtrl', function($scope){
   //   console.log('app ctrl is loaded');
   //   $scope.title = "heroes";
-
   //   $scope.heroes = [
   //     'Superman',
   //     'Batman',
   //     'Catwoman',
   //     'Spiderman'
   //     ];
-
   //   $scope.buttonClicked = function () {
   //     alert('You clicked this button');
   //   };
@@ -17,9 +15,35 @@ angular.module('hipstrchat')
   .controller('HeaderCtrl', function($scope){
     $scope.title = "This is the header";
   })
-  .controller('SidebarCtrl', function($scope){
+  .controller('SidebarCtrl', function($http, $scope){
     $scope.title = "This is the sidebar";
+    $scope.users = [];
+    var getUsers = function () {
+      $http.get('http://hipstrchat.herokuapp.com/users')
+        .success(function(data){
+          console.log(data);
+          $scope.users = data;
+          window.users = $scope.users;
+        })
+        .error(function(data){
+          console.log('error', data);
+          return data;
+        });
+    };
+    getUsers();
   })
-  .controller('MessagesCtrl', function($scope){
+  .controller('MessagesCtrl', function($scope, $http){
     $scope.title = "This is the messages";
+    $scope.messages = [];
+    var getMessages = function () {
+      $http.get('http://hipstrchat.herokuapp.com/rooms/1')
+        .success(function(data){
+          console.log('messages',data);
+          $scope.messages = data.messages;
+        })
+        .error(function(){
+          console.log('error');
+        })
+    };
+    getMessages();
   });
